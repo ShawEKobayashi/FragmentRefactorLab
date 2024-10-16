f2da7d2c8c9f5ae25b9119f687112bc9fb0bb998
tuk60682 <kobayashi.shaw.e@gmail.com>
2024-10-16 16:50:52 -0400

    Moved the code from MainActivity onCreateView() to fragment's onCreateView's inflated layout.apply fun. Added the components and constraints to fragment layout.

--- app/src/main/java/edu/temple/fragmentrefactor/WelcomePage.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/WelcomePage.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,7 +23,21 @@ class WelcomePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_page, container, false)
-    }
+        return inflater.inflate(R.layout.fragment_welcome_page, container, false).apply {
+            val displayTextView = findViewById<TextView>(R.id.textView)
+            val nameEditText = findViewById<EditText>(R.id.editTextText)
+            val changeButton = findViewById<Button>(R.id.button)
 
+            changeButton.setOnClickListener {
+                val name = nameEditText.text
+
+                displayTextView.text = if (name.isNotBlank()) {
+                    "Hello, $name!"
+                } else {
+                    "Please enter your name"
+                }
+
+            }
+        }
+    }
 }
